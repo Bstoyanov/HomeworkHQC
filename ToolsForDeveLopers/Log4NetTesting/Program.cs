2a67f1dfc16e096225160bccc551420df5aaec25
Bstoyanov <etboxi@gmail.com>
2015-11-23 12:32:41 +0200

    added feature

---
+++ ToolsForDeveLopers/Log4NetTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Log4NetTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            
+        }
+    }
+
+   
+}
