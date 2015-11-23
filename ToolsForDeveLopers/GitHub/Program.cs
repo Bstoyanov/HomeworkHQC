5fac718b12d28e12fd44bf07406993c6625b5670
Bstoyanov <etboxi@gmail.com>
2015-11-23 13:30:36 +0200

    added 1dimension array

--- ToolsForDeveLopers/GitHub/Program.cs
+++ ToolsForDeveLopers/GitHub/Program.cs
@@ -11,6 +11,8 @@ namespace GitHub
             int currentRow = 0;
             int currentCol = 0;
             FillMapDots(map);
+            var newArray = new int[3];
+            newArray[2] = 32;
 
             while (true)
             {
