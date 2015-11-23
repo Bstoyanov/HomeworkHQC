9cb09e938bc712f1a83cd4b2cae34603c07ca1b4
Bstoyanov <etboxi@gmail.com>
2015-11-23 06:08:18 +0200

    Created Console Mover

--- ToolsForDeveLopers/GitHub/Program.cs
+++ ToolsForDeveLopers/GitHub/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitHub
 {
@@ -10,6 +6,95 @@ namespace GitHub
     {
         static void Main(string[] args)
         {
+            char[,] map = new char[5, 5];
+            FillMapWithDots(map);
+            int currentRow = 0;
+            int currentCol = 0;
+            while (true)
+            {
+                PrintMap(map);
+
+                var pressedKey = Console.ReadKey();
+                try
+                {
+                    switch (pressedKey.Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            MoveLeft(map, --currentCol, currentRow);
+                            break;
+                        case ConsoleKey.RightArrow:
+                            MoveRight(map, ++currentCol, currentRow);
+                            break;
+                        case ConsoleKey.UpArrow:
+                            MoveUp(map, currentCol, --currentRow);
+                            break;
+                        case ConsoleKey.DownArrow:
+                            MoveDown(map, currentCol, ++currentRow);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Command!");
+                            break;
+                    }
+
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Invalid Command!");
+                    currentRow = 0;
+                    currentRow = 0;
+                }
+            }
+
+        }
+
+        private static void PrintMap(char[,] map)
+        {
+            Console.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        private static void MoveDown(char[,] map, int col, int row)
+        {
+            map[row, col] = '0';
+            map[row - 1, col] = '.';
+        }
+
+        private static void MoveUp(char[,] map, int col, int row)
+        {
+            map[row, col] = '0';
+            map[row + 1, col] = '.';
+        }
+
+        private static void MoveRight(char[,] map, int col, int row)
+        {
+            map[row, col] = '0';
+            map[row, col - 1] = '.';
+        }
+
+        private static void MoveLeft(char[,] map, int col, int row)
+        {
+            map[row, col] = '0';
+            map[row, col + 1] = '.';
+        }
+
+
+        private static void FillMapWithDots(char[,] map)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    map[i, j] = '.';
+                }
+            }
         }
     }
 }
